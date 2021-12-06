998ff8e091826ac97ec0f53b032ce435c07882f4
ohinibla <ohinibla@gmail.com>
2021-12-07 02:08:48 +0330

    Connection-based messaging layout

--- background-script.js
+++ background-script.js
@@ -1,51 +1,19 @@
 var ClassWatchList;
 var init_status = "init";
 
-_send();
+let portFromCS;
 
-/**
- * Log acknowledgement and contents of the message to the console.
- */
-function _ACK(message) {
-    /**
-     * if (!(ClassWatchList.includes(message.selected_class))) {
-     * ClassWatchList.push(message.selected_class);
-     * };
-     */
-    ClassWatchList = message.selected_class
-    _send();
-};
- 
-  function sendMessageToTabs(tabs) {
-    for (let tab of tabs) {
-      browser.tabs.sendMessage(
-        tab.id,
-        {
-            selected_class: ClassWatchList,
-            status: init_status
-        }
-      )
-    };
-    console.log(`background-script sending message`);
-  };
+function connected(p) {
+  portFromCS = p;
+  portFromCS.postMessage({greeting: "hi there content script!"});
+  portFromCS.onMessage.addListener(function(m) {
+    portFromCS.postMessage({greeting: "In background script, received message from content script:" + m.greeting});
+  });
+}
 
-  function onError(error) {
-      console.error(`some error occurred: $(error)`);
-  }
+browser.runtime.onConnect.addListener(connected);
 
-  function _send() {
-    browser.tabs.query({
-      currentWindow: true,
-      active: true
-    }).then(sendMessageToTabs).catch(onError);
-};
-
-/**
- * Send initial message with the proper status and ClassWatchList.
- */
-
-/**
- * Listen for message from the content script.
- */
-browser.runtime.onMessage.addListener(_ACK);
+browser.browserAction.onClicked.addListener(function() {
+  portFromCS.postMessage({greeting: "they clicked the button!"});
+});
 
--- content_script.js
+++ content_script.js
@@ -1,69 +1,50 @@
 /**
- * Send content-script start message to background-script
+ * Adds custom notification button to every examBox > BtnHolder.
  */
+function addCustomButton() {
+    let exam_boxes = document.querySelectorAll(".examBox");
 
-
-(function() {
-
-    /**
-     * Avoid injecting duplicate scripts.
-     */
-    if (window.hasRun) {
-        return;
+    for (let box of exam_boxes) {
+        let CustomButton = document.createElement("a");
+        CustomButton.classList.add("notification-button");
+        CustomButton.classList.add("btn", "examBtn", "contentBtn");
+        CustomButton.style.backgroundColor = "red";
+        CustomButton.textContent = "ورود استاد را به من اطلاع بده";
+        box.getElementsByClassName("btnHolder")[0].prepend(CustomButton);
     }
-    window.hasRun = true;
-
-    /**
-     * Adds custom notification button to every examBox > BtnHolder.
-     */
-    function addCustomButton() {
-        let exam_boxes = document.querySelectorAll(".examBox");
-    
-        for (let box of exam_boxes) {
-            let CustomButton = document.createElement("a");
-            CustomButton.classList.add("notification-button");
-            CustomButton.classList.add("btn", "examBtn", "contentBtn");
-            CustomButton.style.backgroundColor = "red";
-            CustomButton.textContent = "ورود استاد را به من اطلاع بده";
-            box.getElementsByClassName("btnHolder")[0].prepend(CustomButton);
-        }
-    }
-    
-    /**
-     * Style the selected button and add 
-     * appropriate classes. 
-     */
-    function selectbtn(e) {
-        e.target.style.backgroundColor = "green";
-        e.target.classList.add("selected-class");
-        document.querySelectorAll(".notification-button").forEach(item => {
-            if (!item.classList.contains("selected-class")) {
-                item.remove();
-            }
-        })
-    }
-
-    /**
-     * Add click even listener to custom notification buttons and
-     * send appropriate message with the title of the selected class to the background script.
-     */
-    function sendSelectedClass() {
-        document.querySelectorAll(".notification-button").forEach(item => {
-            item.addEventListener("click", (e) => {
-                let class_title = e.target.parentNode.parentNode.getElementsByTagName('h4')[0].textContent;
-                browser.runtime.sendMessage({"selected_class": class_title});
-                selectbtn(e);
-            })
-        })
-    };
-
-    addCustomButton();
-    sendSelectedClass();
-
-})();
+}
 
 /**
- * Get enter class button by the class title.
+ * Style the selected button and add 
+ * appropriate classes. 
+ */
+function selectbtn(e) {
+    e.target.style.backgroundColor = "green";
+    e.target.classList.add("selected-class");
+    document.querySelectorAll(".notification-button").forEach(item => {
+        if (!item.classList.contains("selected-class")) {
+            item.remove();
+        }
+    })
+}
+
+/**
+ * Add click even listener to custom notification buttons and
+ * send appropriate message with the title of the selected class to the background script.
+ */
+function sendSelectedClass() {
+    document.querySelectorAll(".notification-button").forEach(item => {
+        item.addEventListener("click", (e) => {
+            let class_title = e.target.parentNode.parentNode.getElementsByTagName('h4')[0].textContent;
+            browser.runtime.sendMessage({"selected_class": class_title,});
+            selectbtn(e);
+        })
+    })
+};
+
+/**
+ * Get class status (زمان جلسه پایان یافته, زمان جلسه فرا نرسیده, ورود دانشجو)
+ * by class title.
  */
 function getClassEnterbtn(class_title) {
     return document.getElementsByClassName("selected-class")[0].parentNode.lastElementChild.textContent;
@@ -71,7 +52,6 @@ function getClassEnterbtn(class_title) {
 
 function handlebtntext(_class) {
     _case = getClassEnterbtn(_class);
-
     if (_case == "زمان جلسه پایان یافته") {
         console.log("DUE BUZZZZZ!!!");
     } else if (_case == "زمان جلسه فرا نرسیده") {
@@ -79,10 +59,16 @@ function handlebtntext(_class) {
     } else if (_case == "ورود دانشجو") {
         console.log("BUZZZZZ!!!!");
     }
-
 };
 
-browser.runtime.onMessage.addListener(message => {
-    console.log(`content-script receiving message: ${message} - status: ${message.status}`);
-    handlebtntext(message.selected_class);
+let myPort = browser.runtime.connect({name:"port-from-cs"});
+myPort.postMessage({greeting: "hello from content script"});
+
+myPort.onMessage.addListener(function(m) {
+  console.log("In content script, received message from background script: ");
+  console.log(m.greeting);
+});
+
+document.body.addEventListener("click", function() {
+  myPort.postMessage({greeting: "they clicked the page!"});
 });
