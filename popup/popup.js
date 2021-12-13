849a671098aa2dee225b011ea5827d17b55d9e58
ohinibla <ohinibla@gmail.com>
2021-12-13 11:01:15 +0330

    popup main page IU

--- popup/popup.js
+++ popup/popup.js
@@ -8,10 +8,10 @@ popup_port.onMessage.addListener(function(m) {
     console.log(`power status: ${power_stat}`);
     if (m.power == "off") {
         document.getElementById("power-button").style.left = "50px";
-        document.getElementById("power-button-container").style.backgroundColor = "#f26674";
+        document.getElementById("power-button-container").style.backgroundColor = "darkgray";
     } else if (m.power == "on") {
         document.getElementById("power-button").style.left = "100px";
-        document.getElementById("power-button-container").style.backgroundColor = "#a0d468";
+        document.getElementById("power-button-container").style.backgroundColor = "#57b45c";
     }
     document.getElementById("selected-class").textContent = m.selected_class;
 })
@@ -21,12 +21,12 @@ power_button.addEventListener("click", function() {
     if (power_stat == "off") {
         power_stat = "on";
         document.getElementById("power-button").style.left = "70px";
-        document.getElementById("power-button-container").style.backgroundColor = "#a0d468";
+        document.getElementById("power-button-container").style.backgroundColor = "#57b45c";
         popup_port.postMessage({"power": power_stat});
     } else if (power_stat == "on") {
         power_stat = "off";
         document.getElementById("power-button").style.left = "20px";
-        document.getElementById("power-button-container").style.backgroundColor = "#f26674";
+        document.getElementById("power-button-container").style.backgroundColor = "darkgray";
         popup_port.postMessage({"power": power_stat});
     }
 })
